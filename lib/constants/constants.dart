6535334d7c3ec8d10208dbf467f955cbee8cdf38
Ali Wajdan <71645554+Raees453@users.noreply.github.com>
2023-04-17 15:25:10 +0500

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,7 +4,7 @@ class Constants {
   static const String apiModel = 'gpt-3.5-turbo';
 
   // Place your key here.
-  static const String apiSecretKey = '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';
+  static const String apiSecretKey = '';
 
   static const String type = 'Type Something...';
   static const String copy = 'Copy Response';
